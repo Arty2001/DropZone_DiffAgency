0b3feb4f69e8379304b92fee8d2538974eba21cf
Arty2001 <athavanth5@gmail.com>
2023-01-31 14:06:30 -0500

    mobileView fixed

--- src/components/DropZoneCountDown.tsx
+++ src/components/DropZoneCountDown.tsx
@@ -1,26 +1,29 @@
 import { Box, Center, Container, Flex, Group, Stack, Text, Title } from "@mantine/core";
+import { useMediaQuery } from '@mantine/hooks';
 import React from "react";
 import DropZoneNumber from "./DropZoneNumber";
 
 export default function DropZoneCountDown() {
+    const largeScreen = useMediaQuery('(min-width: 900px)');
 
     return (
-        <Flex style={{ height: "75vh" }} 
+        
+        <Flex style={{ height: "80vh" }} 
             gap="md"
-            justify="flex-end"
-            align="center"
+            justify= {largeScreen ? "flex-end" : "center"}
+            align={largeScreen ? "center":"flex-end"}
             direction="row"
             wrap="wrap"
-            mr={"10%"}>
+            mr={largeScreen ? "10%" : 0}>
         <Stack align="center">
-            <Title ta="center">LEEZY BOOST 103</Title>
+                <Title ta="center" style={{ fontSize: largeScreen ? 40 : 35 }} >LEEZY BOOST 103</Title>
             <Group pt={10} pb={10}>
                 <DropZoneNumber number="13" value="DAYS" />
                 <DropZoneNumber number="08" value="HOURS" />
                 <DropZoneNumber number="12" value="MINS" />
                 <DropZoneNumber number="05" value="SECS" />
             </Group>
-            <Text ta="center">UNTIL DROP</Text>
+                <Text ta="center" style={{ fontSize: largeScreen ? 25 : 30}}>UNTIL DROP</Text>
         </Stack>
         </Flex>
     );
--- src/components/DropZoneNumber.tsx
+++ src/components/DropZoneNumber.tsx
@@ -1,4 +1,5 @@
 import { Box, Container, Group, Stack, Text } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import React from "react";
 
 interface NumberProps {
@@ -6,21 +7,22 @@ interface NumberProps {
     value: string;
 }
 
-export default function DropZoneNumber({number, value} : NumberProps) {
+export default function DropZoneNumber({ number, value }: NumberProps) {
+    const largeScreen = useMediaQuery('(min-width: 900px)');
 
     return (
-        <Stack align="flex-start" spacing = "xs">
-        <Group spacing="sm">
-        <Box sx={(theme) => ({ backgroundColor: 'black', width: 50, height: 75 })}>
-            <Text ta="center" c="white" sx={(theme) => ({ fontFamily: 'Rozha One', fontWeight: 900, fontSize: 50 })} > {number[0]}</Text>
-        </Box>
-        <Box sx={(theme) => ({ backgroundColor: 'black', width: 50, height: 75 })}>
-            <Text ta="center" c="white" sx={(theme) => ({ fontFamily: 'Rozha One', fontWeight: 900, fontSize: 50 })} > {number[1]}</Text>
-        </Box>
-        </Group >
-        <div style={{width:110}}>
-            <Text ta="center" c="black" sx={(theme) => ({ fontFamily: 'Roboto, sans-serif', fontWeight: 600, fontSize: 25 })} > {value} </Text>
-        </div>
+        <Stack align="flex-start" spacing="xs">
+            <Group spacing="sm">
+                <Box sx={(theme) => ({ backgroundColor: 'black', width: largeScreen ? 45 : 50, height: largeScreen ? 75 : 50 })}>
+                    <Text ta="center" c="white" style={{ fontFamily: 'Rozha One', fontWeight: 900, fontSize: largeScreen ? 50: 30 }} > {number[0]}</Text>
+                </Box>
+                <Box sx={(theme) => ({ backgroundColor: 'black', width: largeScreen ? 45 : 50, height: largeScreen ? 75 : 50 })}>
+                    <Text ta="center" c="white" style={{ fontFamily: 'Rozha One', fontWeight: 900, fontSize: largeScreen ? 50 : 30 }} > {number[1]}</Text>
+                </Box>
+            </Group >
+            <div style={{ width: 110 }}>
+                <Text ta="center" c="black" style={{ fontFamily: 'Roboto, sans-serif', fontWeight: 600, fontSize: largeScreen ? 25 : 20 }} > {value} </Text>
+            </div>
         </Stack>
     );
 }
